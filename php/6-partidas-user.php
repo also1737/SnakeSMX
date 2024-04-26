851567e122f9cb54d535f0bdce98d4b95fd80094
also1737 <albaricoque.sotot@gmail.com>
2024-04-26 16:33:44 +0200

    añadido comentarios explicativos

--- php/6-partidas-user.php
+++ php/6-partidas-user.php
@@ -1,6 +1,9 @@
 <?php
 
+//comprobamos si hay una sesión iniciada
 if (isset($_COOKIE["PHPSESSID"])) {
+    
+    //si hay sesión abierta, mostramos tabla
     $servidor = "192.168.121.90";
     $base_datos = "SnakeSMX";
     $usuario = "proyecto";
@@ -8,13 +11,13 @@ if (isset($_COOKIE["PHPSESSID"])) {
     
     $conn = new mysqli($servidor, $usuario, $password, $base_datos);
     
-    //
+    //con esta consulta recuperamos las 10 partidas con más puntos del usuarios, con el INNER JOIN mostramos el nombre del mapa en vez de su ID
     $consulta = "SELECT Partida_1J.Puntos, Mapas.NombreMapa, Partida_1J.Fecha, Partida_1J.Dificultad FROM Partida_1J INNER JOIN Mapas ON Partida_1J.IDMapa=Mapas.IDMapa  WHERE Usuario = '$_SESSION[Usuario]' ORDER BY Puntos DESC LIMIT 6";
     
+    //realizamos la consulta
     $resultado = $conn->query($consulta, MYSQLI_USE_RESULT);
-    
-    //$resultado = $resultado->fetch_all(MYSQLI_ASSOC);
 
+    //mostramos la primera fila de la tabla
     echo "<table>
             <tr>
                 <th>Puntos</th>
@@ -22,14 +25,15 @@ if (isset($_COOKIE["PHPSESSID"])) {
                 <th>Fecha</th>
                 <th>Dificultad</th>
             </tr>";
-    
-    /*for ($i = 0; $i < $len; ++$i) {
-        echo "<tr><td>".$resultado[$i]["Puntos"]."</td><td>".$resultado[$i]["IDMapa"]."</td><td>".$resultado[$i]["Fecha"]."</td><td>".$resultado[$i]["Dificultad"]."</td></tr>";
-    }*/
 
+    //numero de líneas con datos, lo necesitaremos más tarde
     $len = 0;
 
+    //este bucle se ejecuta tantas veces como filas haya devuelto la consulta
+    //guardamos una fila en la variable $datos cada iteración
     while($datos=$resultado->fetch_array(MYSQLI_ASSOC)){
+        
+        //mostramos una fila de la tabla con todos los datos
         echo'
             <tr>
                 <td>'.$datos["Puntos"].'</td>
@@ -38,21 +42,22 @@ if (isset($_COOKIE["PHPSESSID"])) {
                 <td>'.$datos["Dificultad"].'</td>
             </tr>';
 
+            //incrementamos len
             $len++;
     
     }
 
+    //aquí imprimimos las filas que faltan si la consulta devuelve menos de 6 filas, para esto usamos $len
     for ($i = 0; $i < 6 - $len; ++$i) {
         echo "<tr><td> </td><td> </td><td> </td><td> </td></tr>";
     }
     
+    //cerramos el html de la tabla
     echo "</table>";
 
 } else {
 
+    //si no hay sesión abierta, mostramos esto
     echo "<div class='derecha' >Inicia sesión para ver tus puntuaciones</div>";
 
 }
-
-
-//print_r($resultado);
--- php/filas-leaderboard.php
+++ php/filas-leaderboard.php
@@ -7,13 +7,13 @@
     
     $conn = new mysqli($servidor, $usuario, $password, $base_datos);
     
-    //
+    //con esta consulta recuperamos las 10 partidas con más puntosde todos los usuarios, con el INNER JOIN mostramos el nombre del mapa en vez de su ID
     $consulta = "SELECT Partida_1J.Puntos, Partida_1J.Usuario, Mapas.NombreMapa, Partida_1J.Fecha, Partida_1J.Dificultad FROM Partida_1J INNER JOIN Mapas ON Partida_1J.IDMapa=Mapas.IDMapa ORDER BY Puntos DESC LIMIT 10";
     
+    //realizamos la consulta y guardamos el resultado
     $resultado = $conn->query($consulta, MYSQLI_USE_RESULT);
-    
-    //$resultado = $resultado->fetch_all(MYSQLI_ASSOC);
 
+    //mostramos la primera fila de la tabla
     echo "<table>
             <tr>
                 <th> </th>
@@ -23,15 +23,18 @@
                 <th>Fecha</th>
                 <th>Dificultad</th>
             </tr>";
-    
-    /*for ($i = 0; $i < $len; ++$i) {
-        echo "<tr><td>".$resultado[$i]["Puntos"]."</td><td>".$resultado[$i]["IDMapa"]."</td><td>".$resultado[$i]["Fecha"]."</td><td>".$resultado[$i]["Dificultad"]."</td></tr>";
-    }*/
 
+    //numero de líneas con datos, lo necesitaremos más tarde
     $len = 0;
+
+    //puesto de la puntuación en cuestión
     $puesto = 1;
 
-    while($datos=$resultado->fetch_array(MYSQLI_ASSOC)){
+    //este bucle se ejecuta tantas veces como filas haya devuelto la consulta
+    //guardamos una fila en la variable $datos cada iteración
+    while($datos=$resultado->fetch_array(MYSQLI_ASSOC)) {
+        
+        //mostramos una fila de la tabla con todos los datos
         echo'
             <tr>
                 <th>'.$puesto.'</th>
@@ -42,15 +45,19 @@
                 <td>'.$datos["Dificultad"].'</td>
             </tr>';
 
+            //incrementamos len y puesto
             $len++;
             $puesto++;
     
     }
 
-    for ($i = 0; $i < 6 - $len; ++$i) {
-        echo "<tr><td> </td><td> </td><td> </td><td> </td></tr>";
+    //aquí imprimimos las filas que faltan si la consulta devuelve menos de 10 filas, para esto usamos $len
+    for ($i = 0; $i < 10 - $len; ++$i) {
+        echo "<tr><th>$puesto</th><td> </td><td> </td><td> </td><td> </td><td> </td></tr>";
+        $puesto++;
     }
     
+    //cerramos el html de la tabla
     echo "</table>";
 
 
