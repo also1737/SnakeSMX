caba8d952f14bd94c15f53ff12f03baeb00d71a2
dvillarg <daviid05gi@gmail.com>
2024-05-08 19:00:03 +0200

    cambios menores

--- javascript/juego.js
+++ javascript/juego.js
@@ -4,7 +4,7 @@ var ctx = c.getContext("2d");
 
 var tablerito = new Tablero(c.height,c.width,25);
 var rafa = new Serpiente(15,12);
-var manzana = new Manzana (25,12,25);
+var manzana = new Manzana (75,40,25);
 
 rafa.empezar();
 rafa.dibujar(tablerito.celdas);
