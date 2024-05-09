2eb5c7e2982463125e3f85b9fbdf5048cb8cc9e4
also1737 <albaricoque.sotot@gmail.com>
2024-05-09 17:53:45 +0200

    sesf

--- javascript/boton-incio.js
+++ javascript/boton-incio.js
@@ -1,5 +1,7 @@
 if (document.getElementById("boton_inicio").innerHTML = "Iniciar Sesión") {
+
     document.getElementById("boton_inicio").style = "background-color: #5d9b2e;";
-} else { (document.getElementById("boton_inicio").innerHTML = "Cerrar Sesión") 
+
+} else if (document.getElementById("boton_inicio").innerHTML = "Cerrar Sesión") { 
     document.getElementById("boton_inicio").style = "background-color: #cccccc;";
 }
